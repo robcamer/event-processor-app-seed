00aaeb721619977a07ac23b7d490b40d8bad21d2
Beijie Zhang <beijiezhang@microsoft.com>
2022-11-18 19:43:34 +0000

    Move testUtil to be under test/

--- test/App.Test/Test.cs
+++ test/App.Test/Test.cs
@@ -298,3 +298,28 @@ public class TestEventProcessor : IDisposable
 		Assert.True(!File.Exists(txtFileName));
 	}
 }
+
+public static class TestUtils
+{
+	public static void VerifyLog<T>(Mock<ILogger<T>> mockLogger, LogLevel logLevel, string? message = null, Expression<Func<object, Type, bool>>? matcher = null)
+	{
+		VerifyLog(mockLogger, logLevel, Times.Once(), message, matcher);
+	}
+
+	public static void VerifyLog<T>(Mock<ILogger<T>> mockLogger, LogLevel logLevel, Times times, string? message = null, Expression<Func<object, Type, bool>>? matcher = null)
+	{
+		if (matcher == null)
+		{
+			matcher = (value, type) => string.Equals(message, value.ToString(), StringComparison.InvariantCultureIgnoreCase);
+		}
+
+		mockLogger.Verify(
+					x => x.Log(
+						logLevel,
+						It.IsAny<EventId>(),
+						It.Is<It.IsAnyType>(matcher),
+						It.IsAny<Exception>(),
+						It.IsAny<Func<It.IsAnyType, Exception?, string>>()),
+					times);
+	}
+}
